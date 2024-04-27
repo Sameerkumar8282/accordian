3cfe94cff12bcb6d33d1e3a973d1228beba13ab7
Sameer Kumar <sameerkumar1012.sk@gmail.com>
2024-04-28 00:39:18 +0530

    update in logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,16 @@ const App = () => {
   
   return (
     <div>
-      <CollapsablePanel Title={"Title 1"} Content={"content 1"}/>
-      <CollapsablePanel  Title={"Title 2"} Content={"content 2"} />
+      <CollapsablePanel Title={"Title 1"} initalCollapse={true}>
+        <p>content 1</p>
+        <p>content 2</p>
+        <p>content 3</p>
+      </CollapsablePanel>
+      <CollapsablePanel  Title={"Title 2"} initalCollapse={false}>
+      <p>content 1</p>
+        <p>content 2</p>
+        <p>content 3</p>
+      </CollapsablePanel>
     </div>
   )
 }
--- src/components/CollapsablePanel.jsx
+++ src/components/CollapsablePanel.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 
-const CollapsablePanel = ({Title,Content}) => {
-  const [collapse, setCollapse] = useState(false);
+const CollapsablePanel = ({Title,children,initalCollapse}) => {
+  const [collapse, setCollapse] = useState(initalCollapse);
 
   return (
     <div className=" border border-black w-1/2">
       <div className=" flex">
-        <div>
-          <p>{Title}</p>
-        </div>
-        <div onClick={() => setCollapse(!collapse)}>
+        
+        <div className="flex bg-gray-400 w-full justify-between py-2" onClick={() => setCollapse(!collapse)}>
+          {Title}
           {collapse ? <FaAngleDown /> : <FaAngleUp />}
         </div>
       </div>
       {collapse ? (
-        <div>
-          <p>{Content}</p>
+        <div className=" bg-slate-100">
+          {children}
         </div>
       ) : null}
     </div>
